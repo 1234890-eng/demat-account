bd6f9bf86ed0843f57fa375c4ca11344f3e303b9
Shiva kishor reddy <74125070+1234890-eng@users.noreply.github.com>
2023-12-20 19:24:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+function submitTo1() {
+  alert("you are succesfully registered");
+    window.location.href = 'nextPage.html';
+}
+
+function submitTo2() {
+  alert("you are succesfully registered");
+    window.location.href = 'nextDone.html';
+}
+
+function submitTo3() {
+  alert("you are succesfully registered");
+    window.location.href = 'final.html';
+}
